aee20874ec78a07a16372ba070af1e2f415d0ded
Gennadiy Zlobin <gennad.zlobin@gmail.com>
2010-11-23 01:13:42 +0600

    Can't find templates maybe because of src folder?

--- src/WebDjango/settings.py
+++ src/WebDjango/settings.py
@@ -1,3 +1,4 @@
+import os
 # Django settings for WebDjango project.
 
 DEBUG = True
@@ -78,6 +79,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'WebDjango.urls'
 
 TEMPLATE_DIRS = (
+    #os.path.join(os.path.basename(__file__), 'templates'),           
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- src/WebDjango/urls.py
+++ src/WebDjango/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
-    (r'^WebDjango/', include('WebDjango.MainApp.urls')),
+    (r'^MainApp/', include('WebDjango.MainApp.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
